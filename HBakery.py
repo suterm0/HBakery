27ff75afc3c7d1714ed6ae2fdb121251d5cacb21
dammara <h3246531>
2019-10-14 21:21:54 -0400

    10/14/19 8:50PM - Fixed "averaging" issue. If statement works.

--- HBakery.py
+++ HBakery.py
@@ -10,7 +10,7 @@ candy_list = []
 
 def cookies():
     for cookie in range(1, 7):
-        sales = (input(f"What are the cookie sales for Month {cookie}? > "))
+        sales = int((input(f"What are the cookie sales for Month {cookie}? > ")))
         cookie_list.append(sales)
         print(f"The cookie sale for Month {cookie} is {sales}")
         if cookie > 7:
@@ -19,7 +19,7 @@ def cookies():
 
 def candies():
     for candy in range(1, 7):
-        sales2 = (input(f"What are the candy sales for Month {candy}? > "))
+        sales2 = int((input(f"What are the candy sales for Month {candy}? > ")))
         print(f"The candy sale for Month {candy} is {sales2}")
         candy_list.append(sales2)
         if candy > 7:
@@ -54,6 +54,8 @@ def min_candy():
     print("The lowest candy sale was:", candy_list[:1])
 
 
+# execute: calls functions in sets
+# /start
 cookies()
 candies()
 input("Press ENTER/RETURN to view the compiled data")
@@ -64,20 +66,29 @@ print("Here is the list of candy sales", candy_list)
 print("")
 input("Press ENTER/RETURN")
 print("")
+# Lines 70 through 73 calculate the total and average it out.
 total_cookies = int((cookie_list[0] + cookie_list[1] + cookie_list[2] + cookie_list[3] + cookie_list[4] + cookie_list[5]))
-avg1 = (total_cookies / 6)
+avg1 = (int(total_cookies) / 6) # Cookie Average
 total_candies = int((candy_list[0] + candy_list[1] + candy_list[2] + candy_list[3] + candy_list[4] + candy_list[5]))
-avg2 = (total_candies / 6)
+avg2 = (int(total_candies) / 6) # Candy Average
+# displays the averages
 avg_cookies()
 avg_candies()
 print("")
+input("Press ENTER/RETURN")
+# Reveals the Maximums
 max_cookie()
 max_candy()
 print("")
+input("Press ENTER/RETURN")
+# Reveals the minimums
 min_cookie()
 min_candy()
 print("")
-if avg1 < avg2:
+input("Press ENTER/RETURN")
+# conclusion
+if avg1 < avg2:  # This is the if statement that determines what is mot popular.
     print("Candy is the most popular item we sell.")
 else:
     print("Cookies are the most popular items we sell")
+# Thanks for reading.
