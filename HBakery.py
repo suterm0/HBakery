5f029911f08efa88cb3df18b12d46aea8a3cd684
dammara <h3246531>
2019-10-14 20:51:20 -0400

    10/14/19 8:50PM - Wrote project code

--- HBakery.py
+++ HBakery.py
@@ -1,3 +1,83 @@
 # Markhus Dammar
 # 10/14/19
 # This program is demonstrating lists, loops, and functions with the Bakery Entry Project
+
+
+# init
+cookie_list = []
+candy_list = []
+
+
+def cookies():
+    for cookie in range(1, 7):
+        sales = (input(f"What are the cookie sales for Month {cookie}? > "))
+        cookie_list.append(sales)
+        print(f"The cookie sale for Month {cookie} is {sales}")
+        if cookie > 7:
+            break
+
+
+def candies():
+    for candy in range(1, 7):
+        sales2 = (input(f"What are the candy sales for Month {candy}? > "))
+        print(f"The candy sale for Month {candy} is {sales2}")
+        candy_list.append(sales2)
+        if candy > 7:
+            break
+
+
+def avg_cookies():
+    print(f"The average cookie sales was {avg1}")
+
+
+def avg_candies():
+    print(f"The average candy sales was {avg2}")
+
+
+def max_cookie():
+    cookie_list.sort()
+    print("The highest cookie sale was:", cookie_list[-1])
+
+
+def max_candy():
+    candy_list.sort()
+    print("The highest candy sale was:", candy_list[-1])
+
+
+def min_cookie():
+    cookie_list.sort()
+    print("The lowest cookie sale was:", cookie_list[:1])
+
+
+def min_candy():
+    candy_list.sort()
+    print("The lowest candy sale was:", candy_list[:1])
+
+
+cookies()
+candies()
+input("Press ENTER/RETURN to view the compiled data")
+print("Here is the list of cookies sales", cookie_list)
+print("")
+input("Press ENTER/RETURN")
+print("Here is the list of candy sales", candy_list)
+print("")
+input("Press ENTER/RETURN")
+print("")
+total_cookies = int((cookie_list[0] + cookie_list[1] + cookie_list[2] + cookie_list[3] + cookie_list[4] + cookie_list[5]))
+avg1 = (total_cookies / 6)
+total_candies = int((candy_list[0] + candy_list[1] + candy_list[2] + candy_list[3] + candy_list[4] + candy_list[5]))
+avg2 = (total_candies / 6)
+avg_cookies()
+avg_candies()
+print("")
+max_cookie()
+max_candy()
+print("")
+min_cookie()
+min_candy()
+print("")
+if avg1 < avg2:
+    print("Candy is the most popular item we sell.")
+else:
+    print("Cookies are the most popular items we sell")
