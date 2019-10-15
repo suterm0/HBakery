90fb225cda9fdc24752ba28c33fffc12f9590345
dammara <h3246531>
2019-10-14 21:30:00 -0400

    10/14/19 9:29PM - Code is finalized with comments.

--- HBakery.py
+++ HBakery.py
@@ -4,20 +4,20 @@
 
 
 # init
-cookie_list = []
-candy_list = []
+cookie_list = []  # creates the cookie list
+candy_list = []  # creates the candy list
 
 
-def cookies():
-    for cookie in range(1, 7):
-        sales = int((input(f"What are the cookie sales for Month {cookie}? > ")))
-        cookie_list.append(sales)
-        print(f"The cookie sale for Month {cookie} is {sales}")
-        if cookie > 7:
+def cookies():  # creates a loop to ask how many of the item were sold during each month, for six months
+    for cookie in range(1, 7):  # 'cookie' is the month (1, 2, 3, 4, 5, 6), 'range' defines which month.
+        sales = int((input(f"What are the cookie sales for Month {cookie}? > ")))  # takes user input for the data set
+        cookie_list.append(sales)  # adds the user inputed value into the list
+        print(f"The cookie sale for Month {cookie} is {sales}")  # tells the user what the sale was for that month.
+        if cookie > 7:  # prevents the loop from getting stuck, probably redundant but worth keeping just in case.
             break
 
 
-def candies():
+def candies():  # see cookie definition, it's the same concept but with candy.
     for candy in range(1, 7):
         sales2 = int((input(f"What are the candy sales for Month {candy}? > ")))
         print(f"The candy sale for Month {candy} is {sales2}")
@@ -26,30 +26,30 @@ def candies():
             break
 
 
-def avg_cookies():
+def avg_cookies():  # defines average
     print(f"The average cookie sales was {avg1}")
 
 
-def avg_candies():
+def avg_candies():  # defines average
     print(f"The average candy sales was {avg2}")
 
 
-def max_cookie():
+def max_cookie():  # defines max
     cookie_list.sort()
     print("The highest cookie sale was:", cookie_list[-1])
 
 
-def max_candy():
+def max_candy():  # defines max
     candy_list.sort()
     print("The highest candy sale was:", candy_list[-1])
 
 
-def min_cookie():
+def min_cookie():  # defines min
     cookie_list.sort()
     print("The lowest cookie sale was:", cookie_list[:1])
 
 
-def min_candy():
+def min_candy():  # defines min
     candy_list.sort()
     print("The lowest candy sale was:", candy_list[:1])
 
